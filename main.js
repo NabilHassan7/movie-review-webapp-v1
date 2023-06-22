bb725e8129a5d4d98fbd5905c66a34da87af4df9
NabilHassan7 <fakrulibnehassan@gmail.com>
2023-06-23 04:07:26 +0600

    progress on API call functions

--- main.js
+++ main.js
@@ -6,7 +6,25 @@ let result = document.getElementById("result");
 
 // API fetch functions
 
+// function to get user input and send API call
 let getMovie = () => {
     let movieName = movieNameRef.value; // stores the user title input
     let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${API_KEY}`; /* API call */
+
+    // checking for empty input field
+    if(movieName.length <= 0){
+        result.innerHTML = `<h3 class="empty-field">Please enter a title to search</h3>`;
+    }
+
+    // if user input exists
+    else{
+        fetch(url)
+        .then((res) => res.json)
+        .then((data) => {
+            // if title exists in the API database
+            if(data.Response == "True"){
+                result.innerHTML = ``
+            }
+        })
+    }
 }
